5709abb81ec15f05c80298035a5fc77ec0e94159
fatumepta <fatumepta-git@yandex.ru>
2019-11-10 02:44:30 +0300

    UPD: UserServiceImpl ready

--- src/main/java/jm/fatumepta/springmvc/app/controller/UserController.java
+++ src/main/java/jm/fatumepta/springmvc/app/controller/UserController.java
@@ -1,5 +1,7 @@
 package jm.fatumepta.springmvc.app.controller;
 
+import jm.fatumepta.springmvc.app.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
 
     // show all users
     @GetMapping("/")
--- src/main/java/jm/fatumepta/springmvc/app/service/UserServiceImpl.java
+++ src/main/java/jm/fatumepta/springmvc/app/service/UserServiceImpl.java
@@ -1,33 +1,44 @@
 package jm.fatumepta.springmvc.app.service;
 
+import jm.fatumepta.springmvc.app.dao.UserDAO;
 import jm.fatumepta.springmvc.app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
+@Service
 public class UserServiceImpl implements UserServiceInterface {
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
     @Override
     public void createUser(User user) {
-
+        userDAO.createUser(user);
     }
 
     @Override
     public User getUser(User user) {
-        return null;
+        return userDAO.getUser(user);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userDAO.getAllUsers();
     }
 
     @Override
     public void editUser(User user) {
-
+        userDAO.editUser(user);
     }
 
     @Override
     public void deleteUser(User user) {
-
+        userDAO.deleteUser(user);
     }
 }
