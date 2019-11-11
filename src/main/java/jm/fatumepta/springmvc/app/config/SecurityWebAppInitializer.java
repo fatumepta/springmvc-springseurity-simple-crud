35e710d9661368da54039fd0c95d007d61f43523
fatumepta <fatumepta-git@yandex.ru>
2019-11-11 11:40:07 +0300

    UPD: added spring-security-web and spring-security-config dependencies; UPD: created SecurityWebAppInitializer and Security config classes;

---
+++ src/main/java/jm/fatumepta/springmvc/app/config/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package jm.fatumepta.springmvc.app.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/jm/fatumepta/springmvc/app/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package jm.fatumepta.springmvc.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
